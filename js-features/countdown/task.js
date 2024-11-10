11367d8d285ba6a1e688805ddc7feb2ba40c9501
Sofya Ivanova <sofia146iv@gmail.com>
2024-11-10 21:59:11 +0300

    fix: first homework

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,11 +2,11 @@ const spanTimer = document.getElementById('timer')
 
 setInterval(() => {
     let currentValue = Number(spanTimer.textContent);
-    if (currentValue >= 0) {
+    if (currentValue > 0) {
         currentValue -= 1
         spanTimer.textContent = currentValue;
     }
-    if (currentValue < 0) {
+    if (currentValue === 0) {
         alert('Вы победили в конкурсе!')
     }
 }, 1000)
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,6 +1,5 @@
 (() => {
     let playing = true,
-        activeHole = 1,
         dead = 0,
         lost = 0;
 
@@ -8,20 +7,10 @@
             playing = true;
             document.getElementById('dead').textContent = 0;
             document.getElementById('lost').textContent = 0;
+            dead = 0;
+            lost = 0;
         },
-                getHole = index => document.getElementById(`hole${index}`),
-                deactivateHole = index => getHole( index ).className = 'hole',
-                activateHole = index => getHole( index ).className = 'hole hole_has-mole'
-
-        next = () => setTimeout(() => {
-            if ( !playing ) {
-                return;
-            }
-            deactivateHole( activeHole );
-            activeHole = Math.floor( 1 + Math.random() * 9 );
-            activateHole( activeHole );
-            next();
-        }, 800 );
+        getHole = index => document.getElementById(`hole${index}`);
     for (let i = 1; i<= 9; i++){
         getHole(i).onclick = () => {
             if (playing) {
@@ -45,5 +34,4 @@
             }
         }
     }
-    next();
 })();
